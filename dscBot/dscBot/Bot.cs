367549b70ab42f40d78133e75ce96693218d0c16
Bartlomiej Calus <bartlomiej.calus01@gmial.com>
2022-07-11 23:11:50 +0200

    Bot basic settings

---
+++ dscBot/dscBot/Bot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
+
+namespace dscBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+        public async Task RunA() 
+        {
+            var conf = new DiscordConfiguration
+            {
+                Token = "OTk2MTU0OTEwOTg5MDk5MDA5.GeEaKi.XdaOULrJoSPSrlH5hcyjolFlHzM-ADS-A0axNM",
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                
+            };
+
+            Client = new DiscordClient(conf);
+            Client.Ready += OnClientReady;
+
+            var commandsConf = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { "%" },
+                EnableDms = false
+                
+            };
+            Commands = Client.UseCommandsNext(commandsConf);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+        private Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
+        {  
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ dscBot/dscBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dscBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunA().GetAwaiter().GetResult();
+            
+        }
+    }
+}
